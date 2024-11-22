f9492f12dc6a4fbf206105c64ba664b66f1c1726
Gunj0 <53684919+Gunj0@users.noreply.github.com>
2024-11-22 17:06:38 +0900

    Update config.py

--- 40_PC/Windows/Keyhac/config.py
+++ 40_PC/Windows/Keyhac/config.py
@@ -33,3 +33,44 @@ def configure(keymap):
     keymap_global["U0-Win-J"] = "Win-Left"
     keymap_global["U0-Win-K"] = "Win-Down"
     keymap_global["U0-Win-L"] = "Win-Right"
+
+
+# 以下は弥生PC用
+    # 左ctrlキーをモディファイアキー(U0)に登録
+    keymap.replaceKey("RAlt", 255)
+    keymap.defineModifier(255, "User0")
+
+    keymap_global = keymap.defineWindowKeymap()
+    
+    # カーソル
+    keymap_global[ "U0-I" ] = keymap.InputKeyCommand("Up")
+    keymap_global[ "U0-K" ] = keymap.InputKeyCommand("Down")
+    keymap_global[ "U0-J" ] = keymap.InputKeyCommand("Left")
+    keymap_global[ "U0-L" ] = keymap.InputKeyCommand("Right")
+
+    # カーソル+Shift
+    keymap_global[ "U0-S-I" ] = keymap.InputKeyCommand("S-Up")
+    keymap_global[ "U0-S-K" ] = keymap.InputKeyCommand("S-Down")
+    keymap_global[ "U0-S-J" ] = keymap.InputKeyCommand("S-Left")
+    keymap_global[ "U0-S-L" ] = keymap.InputKeyCommand("S-Right")
+
+    # カーソル+Ctrl
+    keymap_global[ "U0-C-I" ] = keymap.InputKeyCommand("C-Up")
+    keymap_global[ "U0-C-K" ] = keymap.InputKeyCommand("C-Down")
+    keymap_global[ "U0-C-J" ] = keymap.InputKeyCommand("C-Left")
+    keymap_global[ "U0-C-L" ] = keymap.InputKeyCommand("C-Right")
+
+    # カーソル+Ctrl+Shift
+    keymap_global[ "U0-S-C-I" ] = keymap.InputKeyCommand("S-C-Up")
+    keymap_global[ "U0-S-C-K" ] = keymap.InputKeyCommand("S-C-Down")
+    keymap_global[ "U0-S-C-J" ] = keymap.InputKeyCommand("S-C-Left")
+    keymap_global[ "U0-S-C-L" ] = keymap.InputKeyCommand("S-C-Right")
+
+    # カーソル+Win
+    keymap_global[ "U0-LWin-I" ] = keymap.InputKeyCommand("LWin-Up")
+    keymap_global[ "U0-LWin-K" ] = keymap.InputKeyCommand("LWin-Down")
+    keymap_global[ "U0-LWin-J" ] = keymap.InputKeyCommand("LWin-Left")
+    keymap_global[ "U0-LWin-L" ] = keymap.InputKeyCommand("LWin-Right")
+    
+    # DELETE=Ctrl+Back
+    keymap_global[ "U0-Back" ] = keymap.InputKeyCommand("Delete")
